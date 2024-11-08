04231c36a2b414aadbd75bcefab3fd7789467392
Yusuke Kuoka <ykuoka@gmail.com>
2024-11-08 01:33:02 +0000

    Ensure `describe locks` format gracefully handles missing LockHistoryItem

--- deploy/format_test.go
+++ deploy/format_test.go
@@ -42,11 +42,27 @@ func TestFormatProjectDescs(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name: "myproject2",
+			Phases: []PhaseDesc{
+				{
+					Name: "prod",
+					Phase: Phase{
+						Locked: true,
+						// There should be 1 or more LockHistoryItem if Locked is true.
+						// But we intentionally omit it here to test the case where
+						// some bug causes LockHistoryItem to be empty.
+					},
+				},
+			},
+		},
 	}
 
 	require.Equal(t, `myproject1
   prod: Locked (by user1, for for deployment of revision a)
 myproject1-api
   staging: Locked (by user2, for for deployment of revision b)
+myproject2
+  prod: Locked
 `, FormatProjectDescs(projects))
 }
